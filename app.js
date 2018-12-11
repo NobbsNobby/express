14eae18ffbf4c02b11412d51dcb5595fd8729df5
NobbsNobby <art.kotov1@gmail.com>
2018-12-11 13:47:34 +0300

    commit 3

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ app.get('/', function (req, res) {
     res.send('Express server v 2')
 });
 
+app.get("/about", function(request, response){
+
+    response.send("<h1>О сайте</h1>");
+});
+app.get("/contact", function(request, response){
+
+    response.send("<h1>Контакты</h1>");
+});
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
